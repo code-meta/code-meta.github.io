3a67c08a1bf4aa3e4bd8aa6ab4890f95054b1f1a
code-meta <fahim.x@yahoo.com>
2024-11-10 06:30:01 +0600

    feat: initial design

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Nav from "./components/Nav";
+import ProjectCard from "./components/ProjectCard";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <h1>New update add routing feature</h1>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="container mx-auto px-4 h-dvh">
+      <Nav />
 
-export default App
+      <div className="mt-40">
+        <h1 className="text-3xl lg:text-6xl font-bold font-oswald tracking-wide leading-normal lg:leading-relaxed">
+          Web Apps, APIs, Frontend & Backend. <br />
+          Streamlined, Optimized, <br />
+          and Ready to Perform
+        </h1>
+      </div>
+
+      <div className="py-24">
+        <ProjectCard
+          name="Stride Journal"
+          live="https://rumble.com"
+          description="A no-nonsense journaling app built to keep you on track, amplify your habits, and push you toward your goals daily."
+        />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,90 @@
+import * as React from "react";
+import Autoplay from "embla-carousel-autoplay";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { SquareArrowOutUpRight } from "lucide-react";
+import { Link, useNavigate } from "react-router-dom";
+import slugify from "slugify";
+
+interface ProjectCardProps {
+  name: string;
+  live?: string;
+  description: string;
+}
+
+const ProjectCard = ({ name, live = "", description }: ProjectCardProps) => {
+  const plugin = React.useRef(
+    Autoplay({ delay: 2000, stopOnInteraction: true })
+  );
+
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col-reverse lg:flex-row items-center lg:justify-between gap-6 lg:gap-12">
+      <div className="text-center lg:text-left pt-4 lg:pt-0">
+        <Link to={live} target={live !== "" ? "_blank" : "_self"}>
+          <h4 className="text-2xl font-semibold flex items-center justify-center lg:justify-start gap-x-4 cursor-pointer">
+            <span>{name}</span>
+            {live && <SquareArrowOutUpRight />}
+          </h4>
+        </Link>
+        <p className="text-lg mt-2">{description}</p>
+        <button
+          className="bg-green-950 px-4 py-2 rounded-md mt-5"
+          onClick={() => {
+            const link = slugify(name, { lower: true });
+            navigate(`projects/${link}`);
+          }}
+        >
+          View Details
+        </button>
+      </div>
+
+      <div className="flex justify-center lg:justify-start mt-6 lg:mt-0 w-full">
+        <Carousel
+          plugins={[plugin.current]}
+          className="w-full max-w-full"
+          onMouseEnter={plugin.current.stop}
+          onMouseLeave={plugin.current.reset}
+        >
+          <CarouselContent>
+            {[...Array(2)].map((_, idx) => (
+              <CarouselItem key={idx}>
+                <div className="p-1">
+                  <Card className="!bg-transparent border-none">
+                    <CardContent className="flex aspect-square items-center justify-center p-6">
+                      <picture>
+                        <source
+                          media="(min-width: 1024px)"
+                          srcSet={`/images/log-journal/log-large.png`}
+                          className="w-full max-w-[600px] rounded-lg glow-effect"
+                        />
+                        <img
+                          src="/images/log-journal/log-medium.png"
+                          alt="Responsive content"
+                          className="w-full max-w-[600px] rounded-lg glow-effect"
+                        />
+                      </picture>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+
+          {/* Add the navigation arrows here */}
+          <CarouselPrevious className="absolute top-1/2 -translate-y-1/2 left-2 bg-black/40 text-gray-400 rounded-full p-2 hover:bg-black/60 border-green-950" />
+          <CarouselNext className="absolute top-1/2 -translate-y-1/2 right-2 bg-black/40 text-gray-400 rounded-full p-2 hover:bg-black/60 border-green-950" />
+        </Carousel>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
